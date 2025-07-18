09411e753c26d0a27b6d6702d5004d6830e2d060
transcendentalbot <admin@transcendentalbot.net>
2025-07-17 21:04:45 -0400

    Add lib/develop-basic-landing-page-0.ts for Develop: Basic Landing Page

---
+++ lib/develop-basic-landing-page-0.ts
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import { Navigation } from '@/components/Navigation';
+import { HeroSection } from '@/components/HeroSection';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
